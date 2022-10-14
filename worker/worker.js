04063fd1a23bd237206d82d097f5daaed225b800
Raghav Tiruvallur <rtiruvallur@gmail.com>
2022-10-14 23:27:57 +0530

    master sends files to the worker nodes that they are incharge of

--- worker/worker.js
+++ worker/worker.js
@@ -4,4 +4,12 @@ router.get("/are-you-free",(req,res)=>{
     res.status(200).json({"data":"free"})
 })
 
+router.post("/mapping",(req,res)=>{
+    const files=req.body.data
+    
+    res.status(200).json({"data":"received"})
+})
+
+
+
 module.exports=router
